e8d530c222c1ada3273ceb3bab1beb8ce90a8d99
lennert <lennert@lennerts-MacBook-Air.local>
2023-10-01 22:50:12 +0200

    restructure the code and add URL stuff

--- script.js
+++ script.js
@@ -1,25 +1,16 @@
+// Color ranges for victims
 const colorRanges = [
-    { min: 10000, max: Infinity, color: '#800026' },
-    { min: 5000, max: 10000, color: '#BD0026' },
-    { min: 2000, max: 5000, color: '#E31A1C' },
-    { min: 1000, max: 2000, color: '#FC4E2A' },
-    { min: 500, max: 1000, color: '#FD8D3C' },
-    { min: 200, max: 500, color: '#FEB24C' },
-    { min: 100, max: 200, color: '#FED976' },
-    { min: 0, max: 100, color: '#FFEDA0' }
+    { min: 10000, color: '#800026' },
+    { min: 5000, color: '#BD0026' },
+    { min: 2000, color: '#E31A1C' },
+    { min: 1000, color: '#FC4E2A' },
+    { min: 500, color: '#FD8D3C' },
+    { min: 200, color: '#FEB24C' },
+    { min: 100, color: '#FED976' },
+    { min: 0, color: '#FFEDA0' }
 ];
 
-const getColor = (victims) => {
-    const lightGray = 'rgb(220, 220, 220)';
-    if (victims === null) return lightGray;
-    for (const range of colorRanges) {
-        if (victims > range.min) {
-            return range.color;
-        }
-    }
-    return lightGray;
-};
-
+// Custom coordinates for countries
 const customCoordinates = {
     'United States of America': [37.0902, -95.7129],
     'France': [46.603354, 1.888334],
@@ -40,47 +31,33 @@ const customCoordinates = {
     'Argentina': [-38.4161, -63.6167]
 };
 
+// Initialize map
 const map = L.map('map').setView([0, 0], 2);
 
-const isOverlapping = (element1, element2) => {
-    const rect1 = element1.getBoundingClientRect();
-    const rect2 = element2.getBoundingClientRect();
-    return !(
-        rect1.right < rect2.left ||
-        rect1.left > rect2.right ||
-        rect1.bottom < rect2.top ||
-        rect1.top > rect2.bottom
-    );
-};
-
-// Function to remove overlapping tooltips with fewer victims
-const removeOverlappingTooltips = (tooltips) => {
-    for (let i = 0; i < tooltips.length; i++) {
-        for (let j = i + 1; j < tooltips.length; j++) {
-            const tooltip1 = tooltips[i];
-            const tooltip2 = tooltips[j];
-            if (isOverlapping(tooltip1.element, tooltip2.element)) {
-                if (tooltip1.victims < tooltip2.victims) {
-                    tooltip1.tooltip.remove();
-                    tooltips.splice(i, 1);
-                    i--;
-                    break;
-                } else {
-                    tooltip2.tooltip.remove();
-                    tooltips.splice(j, 1);
-                    j--;
-                }
-            }
-        }
-    }
-};
-
-// Array to store tooltip elements and their victim counts
+// Initialize tooltip data arrays
 let tooltipData = [];
 let initialTooltipData = [];
 
+// Helper Functions
+const getColor = victims => colorRanges.find(range => victims > range.min)?.color || 'rgb(220, 220, 220)';
+const isOverlapping = (el1, el2) => !(el1.getBoundingClientRect().right < el2.getBoundingClientRect().left ||
+    el1.getBoundingClientRect().left > el2.getBoundingClientRect().right ||
+    el1.getBoundingClientRect().bottom < el2.getBoundingClientRect().top ||
+    el1.getBoundingClientRect().top > el2.getBoundingClientRect().bottom);
 
-const isURL = (str) => {
+const removeOverlappingTooltips = tooltips => {
+    tooltips.forEach((t1, i) => {
+        tooltips.forEach((t2, j) => {
+            if (i !== j && isOverlapping(t1.element, t2.element)) {
+                const minVictimTooltip = t1.victims < t2.victims ? t1 : t2;
+                minVictimTooltip.tooltip.remove();
+                tooltips.splice(tooltips.indexOf(minVictimTooltip), 1);
+            }
+        });
+    });
+};
+
+const isURL = str => {
     try {
         new URL(str);
         return true;
@@ -88,6 +65,7 @@ const isURL = (str) => {
         return false;
     }
 };
+
 
 const uniqueReferences = (references) => {
     let urlMap = new Map();
@@ -116,110 +94,55 @@ const uniqueReferences = (references) => {
     return [urlLinks, textRefs].filter(Boolean).join(', ');
 };
 
-fetch('./data.json')
-    .then((response) => response.json())
-    .then((data) => {
-        fetch('https://raw.githubusercontent.com/datasets/geo-countries/master/data/countries.geojson')
-            .then(response => response.json())
-            .then((geojson) => {
-                L.geoJSON(geojson, {
-                    style: (feature) => {
-                        const countryData = data.find(item => item.country === feature.properties.ADMIN);
-                        return {
-                            fillColor: getColor(countryData ? countryData.victims : null),
-                            weight: 0.5,
-                            opacity: 1,
-                            color: 'white',
-                            fillOpacity: 1
-                        };
-                    },
-                    onEachFeature: (feature, layer) => {
-                        const countryData = data.find(item => item.country === feature.properties.ADMIN);
-                        if (countryData && countryData.victims !== null) {
-                            const formattedVictims = countryData.victims.toLocaleString();  // Format the number
-                            let latlng;
-                            if (customCoordinates[countryData.country]) {
-                                latlng = customCoordinates[countryData.country];
-                            } else {
-                                const bounds = layer.getBounds();
-                                latlng = bounds.getCenter();
-                            }
+const createPopupContent = (countryData) => {
+    const formattedVictims = countryData.victims.toLocaleString();
+    const referencesHtml = countryData.references ? `Reference: ${uniqueReferences(countryData.references)}` : '';
+    return `<b>${countryData.country}</b><br>Victims: ${formattedVictims}<br>${referencesHtml}`;
+};
 
-                            let referencesHtml = '';
-                            if (countryData.references) {
-                                referencesHtml = 'Reference: ' + uniqueReferences(countryData.references);
-                            }
+const createAndStoreTooltip = (latlng, formattedVictims) => {
+    const tooltip = L.tooltip({
+        permanent: true,
+        className: 'country-label',
+        offset: [0, 0],
+        direction: 'center'
+    }).setLatLng(latlng).setContent(formattedVictims).addTo(map);
 
-                            layer.bindPopup(`<b>${countryData.country}</b><br>Victims: ${formattedVictims}<br>${referencesHtml}`, {
-                                offset: L.point(0, -10),
-                            });
+    const tooltipElement = tooltip.getElement();
+    const tooltipInfo = { tooltip, victims: formattedVictims, element: tooltipElement };
 
-                            layer.on('mousemove', function (e) {
-                                const latlng = e.latlng;
-                                const popup = this.getPopup();
-                                popup.options.offset = L.point(0, 10);
-                                // this.setLatLng(latlng).openPopup();
-                            });
-                            const tooltip = L.tooltip({
-                                permanent: true,
-                                className: 'country-label',
-                                offset: [0, 0],
-                                direction: 'center'
-                            })
-                                .setLatLng(latlng)
-                                .setContent(formattedVictims)
-                                .addTo(map);
-                            const tooltipElement = tooltip.getElement();
-                            const tooltipInfo = { tooltip, victims: countryData.victims, element: tooltipElement };
-                            initialTooltipData.push(tooltipInfo); // Store initial tooltips
-                            tooltipData.push(tooltipInfo); // Store tooltips currently displayed
+    initialTooltipData.push(tooltipInfo);
+    tooltipData.push(tooltipInfo);
+};
 
-                        }
-                    }
-                }).addTo(map);
+// Fetch and Render Data
+const fetchDataAndRender = async () => {
+    const data = await fetch('./data.json').then(res => res.json());
+    const geojson = await fetch('https://raw.githubusercontent.com/datasets/geo-countries/master/data/countries.geojson').then(res => res.json());
+    L.geoJSON(geojson, {
+        style: feature => ({
+            fillColor: getColor(data.find(item => item.country === feature.properties.ADMIN)?.victims),
+            weight: 0.5,
+            opacity: 1,
+            color: 'white',
+            fillOpacity: 1
+        }),
+        onEachFeature: (feature, layer) => {
+            const countryData = data.find(item => item.country === feature.properties.ADMIN);
+            if (countryData?.victims) {
+                const latlng = customCoordinates[countryData.country] || layer.getBounds().getCenter();
+                const popupContent = createPopupContent(countryData);
 
+                layer.bindPopup(popupContent, {
+                    offset: L.point(0, -10)
+                });
 
-                // // Step 1: Calculate the total victims per continent
-                // let continentTotals = {};
-                // data.forEach(item => {
-                //     if (item.continent && item.victims !== null) {
-                //         if (!continentTotals[item.continent]) {
-                //             continentTotals[item.continent] = 0;
-                //         }
-                //         continentTotals[item.continent] += item.victims;
-                //     }
-                // });
+                createAndStoreTooltip(latlng, countryData.victims.toLocaleString());
+            }
+        }
+    }).addTo(map);
+};
 
-                // // Sort continents by the number of victims
-                // const sortedContinents = Object.entries(continentTotals).sort((a, b) => b[1] - a[1]);
-
-                // // Mapping of continents to emojis
-                // const continentEmojis = {
-                //     'Africa': '🌍',
-                //     'Asia': '🌏',
-                //     'Europe': '🌍',
-                //     'North America': '🌎',
-                //     'Latin America': '🌎',
-                //     'Oceania': '🌏'
-                // };
-
-                // // Update the HTML to display total victims per continent
-                // const continentTotalsDiv = document.getElementById('continent-totals');
-                // let htmlContent = '<ul>';
-                // sortedContinents.forEach(([continent, total]) => {
-                //     const emoji = continentEmojis[continent] || '';
-                //     htmlContent += `<li>${emoji} ${continent}: ${total.toLocaleString()} victims</li>`;
-                // });
-                // htmlContent += '</ul>';
-                // continentTotalsDiv.innerHTML = htmlContent;
-
-
-            }).then(() => {
-                removeOverlappingTooltips(tooltipData);
-            });
-
-
-    });
 
 // Function to re-add all tooltips based on current zoom level
 const reAddTooltips = (tooltips) => {
@@ -274,3 +197,29 @@ legend.onAdd = function (map) {
 
 legend.addTo(map);
 
+const updateURL = () => {
+    const { lat, lng } = map.getCenter();
+    const zoom = map.getZoom();
+    window.location.hash = `${zoom}/${lat.toFixed(6)}/${lng.toFixed(6)}`;
+};
+
+// Function to set map view from the URL hash
+const setMapViewFromHash = () => {
+    const hash = window.location.hash.replace('#', '');
+    if (hash !== '') {
+        const [zoom, lat, lng] = hash.split('/');
+        if (zoom && lat && lng) {
+            map.setView([parseFloat(lat), parseFloat(lng)], parseInt(zoom));
+        }
+    }
+};
+
+// Update the URL hash whenever the map view changes
+map.on('moveend', updateURL);
+
+// Set the map view from the URL hash when the page loads
+window.addEventListener('load', setMapViewFromHash);
+
+fetchDataAndRender().then(() => {
+    removeOverlappingTooltips(tooltipData);
+});
